6ab80249d158498de972735eaa1587b7c1e65923
Jack Patton <jackpatton@Jacks-MacBook-Pro.local>
2020-06-07 11:06:18 -0500

    added date picker widget

--- lib/views/new_parties/date_view.dart
+++ lib/views/new_parties/date_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:partyApp/models/party.dart';
 import 'package:partyApp/views/new_parties/details_view.dart';
+import 'package:date_range_picker/date_range_picker.dart' as DateRangePicker;
+import 'dart:async';
 
-class NewPartyDateView extends StatelessWidget {
+class NewPartyDateView extends StatefulWidget {
   final Party party;
   NewPartyDateView({Key key, @required this.party}) : super(key: key);
+
+  @override
+  _NewPartyDateViewState createState() => _NewPartyDateViewState();
+}
+
+class _NewPartyDateViewState extends State<NewPartyDateView> {
+
+  DateTime _date = DateTime.now();
+
+  Future displayDateRangePicker(BuildContext ctxt) async {
+    final List<DateTime> picked = await DateRangePicker.showDatePicker(
+      context: ctxt,
+      initialFirstDate: _date,
+      initialLastDate: _date,
+      firstDate: new DateTime(DateTime.now().year - 50),
+      lastDate: new DateTime(DateTime.now().year + 50));
+      if(picked != null) {
+        setState(() {
+          _date = picked[0];
+        });
+      }
+  }
 
   @override
   Widget build(BuildContext ctxt){
@@ -22,14 +46,18 @@ class NewPartyDateView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text("Enter A Date*"),
-              
+              RaisedButton(
+                child: Text("Select Date"),
+                onPressed: () async {
+                await displayDateRangePicker(ctxt);
+              }),
               RaisedButton(
                 child: Text("Continue"),
                 onPressed: () {
-                  party.date = DateTime.now();
+                  widget.party.date = _date;
                   Navigator.push(
                     ctxt,
-                    MaterialPageRoute(builder: (ctxt) => NewPartyDetailsView(party: party)),
+                    MaterialPageRoute(builder: (ctxt) => NewPartyDetailsView(party: widget.party)),
                   );
                 },
               ),
